2742a240a9e2502d4425fc2ed73f7d19e0f725c7
loc-nguyen-qe <locnguyent31088@gmail.com>
2025-09-12 23:41:39 +0700

    adjust test data

--- tests/regression_test/SearchTest.spec.ts
+++ tests/regression_test/SearchTest.spec.ts
@@ -2,14 +2,14 @@ import {test} from "../../data/fixtures/pageFixture";
 
 
 test('Search product with exacts keyword', async ({homePage}) => {
-    await homePage.searchComponent.searchWithKeyword('Samsung')
+    await homePage.searchComponent.searchWithKeyword('Samsung Premium Ultrabook')
     await homePage
         .searchComponent
         .isSuggestionItemDisplayed()
 })
 
 test('Search product with partial keyword', async ({homePage}) => {
-    await homePage.searchComponent.searchWithKeyword('Samsung')
+    await homePage.searchComponent.searchWithKeyword('Nike')
     await homePage
         .searchComponent
         .isSuggestionItemDisplayed()
