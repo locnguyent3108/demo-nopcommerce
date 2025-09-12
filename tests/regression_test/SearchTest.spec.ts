ac6be2bf3da13739b3401400ffb225889c365018
loc-nguyen-qe <locnguyent31088@gmail.com>
2025-09-12 23:34:02 +0700

    move test case search form home page to search test

--- tests/regression_test/SearchTest.spec.ts
+++ tests/regression_test/SearchTest.spec.ts
@@ -1,8 +1,11 @@
 import {test} from "../../data/fixtures/pageFixture";
 
 
-test('Search product with exacts keyword', async ({}) => {
-    await searchComp
+test('Search product with exacts keyword', async ({homePage}) => {
+    await homePage.searchComponent.searchWithKeyword('Samsung')
+    await homePage
+        .searchComponent
+        .isSuggestionItemDisplayed()
 })
 
 test('Search product with partial keyword', async ({}) => {
