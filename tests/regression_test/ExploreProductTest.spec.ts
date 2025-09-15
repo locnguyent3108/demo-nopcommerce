a86976629d022b3a806c061503182d4dea6e8299
loc-nguyen-qe <locnguyent31088@gmail.com>
2025-09-16 00:00:59 +0700

    remove .only()

--- tests/regression_test/ExploreProductTest.spec.ts
+++ tests/regression_test/ExploreProductTest.spec.ts
@@ -2,12 +2,12 @@ import { test } from "../../data/fixtures/pageFixture";
 import { product } from "../../pages/common/ProductMenu";
 import { productType } from "../../types/type";
 import homePageObject from "../../pages/HomePageObject";
-import {expect} from "@playwright/test";
+import { expect } from "@playwright/test";
 
 test.describe('ExploreProduct flow', async () => {
     let productToExplore: productType
     let productToVerifyExplore: string[]
-    test.beforeEach('Prep-condition', async ({page, homePage}) => {
+    test.beforeEach('Prep-condition', async ({ page, homePage }) => {
         await page.goto('')
         productToExplore = homePage
             .headerComponent
@@ -19,7 +19,7 @@ test.describe('ExploreProduct flow', async () => {
             .productMenu
             .getParentProductItem('Computers')
     })
-    test('Explore product by category @component', async ({homePage, productDetails}) => {
+    test('Explore product by category @component', async ({ homePage, productDetails }) => {
         const specificProduct = await homePage.selectProductFromHeader(productToExplore)
         await productDetails
             .isProductPageDisplay(specificProduct)
@@ -28,32 +28,32 @@ test.describe('ExploreProduct flow', async () => {
 
     })
 
-    test('Explore product by featured products @component', async ({homePage, productDetails}) => {
+    test('Explore product by featured products @component', async ({ homePage, productDetails }) => {
         await homePage.selectRandomInFeaturedProducts()
         await productDetails.productDetailsShouldDisplay()
     })
 
-    test('Sort by price', async ({productDetails, homePage, page}) => {
+    test('Sort by price', async ({ productDetails, homePage, page }) => {
         const specificProduct = await homePage.selectProductFromHeader(productToExplore)
         await productDetails.sortByPrice()
         await productDetails.verifyProductOrderByPriceFromLowToHigh()
     })
 
     for (const manuItem of ['Apple', 'HP']) {
-        test(`Filter by manufacture:  ${manuItem}`, async ({homePage, productDetails}) => {
+        test(`Filter by manufacture:  ${manuItem}`, async ({ homePage, productDetails }) => {
             const specificProduct = await homePage.selectProductFromHeader(productToExplore)
             await productDetails.selectManufacture('Apple')
             await productDetails.isProductPageDisplay('Apple')
         })
     }
-        test.only(`Filter by price`, async ({page, homePage, productDetails}) => {
-            const productToTest = 'desktops'
-            await page.goto(productToTest)
-            await productDetails.filterByPriceViaUrl(productToTest, 9000, 9500)
-            await productDetails.emptyPageShouldDisplay()
+    test(`Filter by price`, async ({ page, homePage, productDetails }) => {
+        const productToTest = 'desktops'
+        await page.goto(productToTest)
+        await productDetails.filterByPriceViaUrl(productToTest, 9000, 9500)
+        await productDetails.emptyPageShouldDisplay()
 
-            await productDetails.filterByPriceViaUrl(productToTest, 20, 9500)
-            await productDetails.isAllItemDetailsShouldNotBroken()
-        })
+        await productDetails.filterByPriceViaUrl(productToTest, 20, 9500)
+        await productDetails.isAllItemDetailsShouldNotBroken()
+    })
 
 })
